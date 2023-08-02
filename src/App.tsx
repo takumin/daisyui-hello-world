f7eb1293f8c27565fb83337c2c7a07d06919c441
takumin <takumiiinn@gmail.com>
2023-08-02 12:28:08 +0900

    add daisyui example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import './App.css'
+
 function App() {
   return (
     <>
-      <h1>daisyUI Hello World</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-5xl">
+            <h1 className="text-5xl font-bold">daisyUI Hello World</h1>
+            <p className="py-6">test text</p>
+            <button className="btn btn-primary">Get hear</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
